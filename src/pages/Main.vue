c2a00b4a35e4189da41246653e13daaef6065b24
billy7175 <whgus439@gmail.com>
2023-12-05 15:38:39 +0900

    feat: add main.vue component

---
+++ src/pages/Main.vue
@@ -0,0 +1,22 @@
+<script setup>
+import NavMenu from '../components/NavMenu.vue'
+</script>
+
+<template>
+  <NavMenu></NavMenu>
+  <main class="main">
+    <div class="main__inner">
+      <router-view></router-view>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.main__inner {
+  width:100%;
+  border:1px solid rgb(127, 54, 54);
+  padding:20px;
+  height:auto;
+} 
+
+</style>
