f44e83e9d67f672bd7d2f42ccffdaaaf03876482
LAPTOP-C71E3L3P\Billy <whgus439@gmail.com>
2023-12-12 02:02:43 +0900

    set axios withCredentials globally

--- src/apis/login.js
+++ src/apis/login.js
@@ -1,34 +1,30 @@
-import axios from 'axios'
+import axios from "axios";
 
 const getTest = async () => {
-    try {
-        console.log('#login')
-        const response = await axios.get('http://127.0.0.1:3000/api', {
-            withCredentials: true
-        })
-        console.log('#response', response)
-        return response
-    } catch (error) {
-        console.log('#error', error)
-        throw error
-    }
-}
+  try {
+    console.log("#login");
+    const response = await axios.get("http://127.0.0.1:3000/api");
+    console.log("#response", response);
+    return response;
+  } catch (error) {
+    console.log("#error", error);
+    throw error;
+  }
+};
 
 const loginPost = async (params) => {
   try {
     console.log("#loginPost", params);
-    const response = await axios.post('http://127.0.0.1:3000/api/login', params, {
-        withCredentials: true
-      });
-      console.log('#response', response)
-      return response
-} catch (error) {
-    console.log('#loginPost', error)
-    throw error
-}
-}
+    const response = await axios.post(
+      "http://127.0.0.1:3000/api/login",
+      params
+    );
+    console.log("#response", response);
+    return response;
+  } catch (error) {
+    console.log("#loginPost", error);
+    throw error;
+  }
+};
 
-export {
-getTest,
-loginPost
-}
+export { getTest, loginPost };
--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 // import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import './assets/main.css'
+import "element-plus/dist/index.css";
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import routers from './routers/index.js'
-import { createPinia} from 'pinia'
-const pinia = createPinia()
+import { createApp } from "vue";
+import App from "./App.vue";
+import routers from "./routers/index.js";
+import { createPinia } from "pinia";
+import axios from "./apis/index.js";
+const pinia = createPinia();
 
-const app = createApp(App)
-app.use(routers) 
-app.use(pinia)
-app.mount('#app')
+const app = createApp(App);
+
+app.use(axios);
+app.use(routers);
+app.use(pinia);
+
+app.mount("#app");
