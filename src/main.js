c90c630940bc1a739f440bada43b1ce35ad2256e
billy7175 <whgus439@gmail.com>
2023-12-15 17:01:34 +0900

    add element ui icons

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // import ElementPlus from 'element-plus'
 import "element-plus/dist/index.css";
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import "./assets/main.css";
 
 import { createApp } from "vue";
@@ -34,5 +35,10 @@ if (token) {
         user
     })
 }
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
+  
 
 app.mount("#app");
