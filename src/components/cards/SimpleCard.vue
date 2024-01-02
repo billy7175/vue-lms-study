91e9977a5d1a64feba0d24b4961dccbed560c4b6
billy7175 <whgus439@gmail.com>
2024-01-03 03:13:30 +0900

    styles dashboard card component

--- src/components/cards/SimpleCard.vue
+++ src/components/cards/SimpleCard.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="simple-card">
-    <p class="simple-card__title">
-      {{ title }}
-    </p>
+  <div class="simple-card" :class="[type]">
+    <p class="simple-card__title">{{ title }}</p>
     <div class="simple-card__contents">
-      <span class="simple-card__contents-text">{{ contents }}</span>
+      <span class="simple-card__contents-text" :class="[type]">{{
+        contents
+      }}</span>
     </div>
   </div>
 </template>
@@ -14,19 +14,23 @@
 const props = defineProps({
   title: {
     type: String,
-    default: "title",
+    default: "",
   },
   contents: {
     type: String,
-    default: "contents",
+    default: "",
+  },
+  type: {
+    type: String,
+    default: "",
   },
 });
 </script>
 
 <style scoped>
 .simple-card {
-  width: 250px;
-  height: 150px;
+  width: 300px;
+  height: 180px;
   box-shadow: 0 5px 10px rgba(154, 160, 185, 0.05),
     0 15px 40px rgba(166, 173, 201, 0.2);
   border-radius: 10px;
@@ -39,6 +43,7 @@ const props = defineProps({
 .simple-card__title {
   display: flex;
   align-items: center;
+  font-weight: 700;
 }
 
 .simple-card__contents {
@@ -49,16 +54,51 @@ const props = defineProps({
 
 .simple-card__contents-text {
   position: relative;
-  color: #f1c40f;
+  padding: 10px;
+  border-radius: 50%;
+  color: #000;
 }
+
+.simple-card.type-01 {
+  background: #d3e3fd;
+}
+
+.simple-card__contents-text.type-01 {
+  background: #a8c7fa;
+}
+
+.simple-card.type-02 {
+  background: #E3E3E3;
+}
+
+.simple-card__contents-text.type-02 {
+  background: #C7C7C7;
+}
+
+
+.simple-card.type-03 {
+  background: #C2E7FF
+}
+
+.simple-card__contents-text.type-03 {
+  background: #7FCFFF;
+}
+
+.simple-card.type-04 {
+  background: #C4EED0;
+}
+
+.simple-card__contents-text.type-04 {
+  background: #6DD58C;
+}
+
 .simple-card__contents-text::before {
   content: "";
   position: absolute;
   top: 100%;
   left: 0px;
-  background: #f1c40f;
   width: 100%;
   height: 2px;
-  opacity: .6;
+  opacity: 0.6;
 }
 </style>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,15 +1,49 @@
 <template>
-    <div class="dashboard">
-        <div>
-            <simple-card title="prop title"></simple-card>
-        </div>
-    </div>
+  <div class="dashboard">
+    <section>
+      <h2>Analysis</h2>
+      <div class="card__wrapper">
+        <simple-card
+          title="학생 수"
+          contents="33명"
+          type="type-01"
+        ></simple-card>
+        <simple-card
+          title="선생 수"
+          contents="2명"
+          type="type-02"
+        ></simple-card>
+        <simple-card title="반" contents="3개" type="type-03"></simple-card>
+        <simple-card title="생성된 문제 수" contents="3개" type="type-04"></simple-card>
+      </div>
+    </section>
+    <section style="margin-top: 50px">
+      <h2>Popular Courses</h2>
+      <div class="card__wrapper">
+        <simple-card title="English Grammmars In Use" contents="13$"></simple-card>
+        <simple-card
+          title="Japanese Grammars In Use"
+          contents="7$"
+          mainBackground="red"
+        ></simple-card>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script setup>
-import SimpleCard from '../components/cards/SimpleCard.vue'
+import SimpleCard from "../components/cards/SimpleCard.vue";
 </script>
 
 <style scoped>
-.dashboard {}
+.card__wrapper {
+  display: flex;
+  gap: 50px;
+  flex-wrap: wrap;
+}
+
+h2 {
+    padding-left:5px;
+    margin-bottom: 5px;
+}
 </style>
