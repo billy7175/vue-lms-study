2203cc356c551f5f60d0df74eeca005d6023e744
billy7175 <whgus439@gmail.com>
2024-01-05 18:27:35 +0900

    add QuestionForm component

--- src/components/form/QuestionForm.vue
+++ src/components/form/QuestionForm.vue
@@ -1,28 +1,85 @@
 <template>
   <div>
     <h5>문제 생성</h5>
-    <Button
-      label="Create Question"
-      icon="pi pi-external-link"
-      @click="openBasic"
-      style="margin-bottom: 50px"
-    />
+    <div style="display:flex; justify-content: flex-end;">
+      <Button
+        label="Create Question"
+        icon="pi pi-external-link"
+        @click="handleModalOpen"
+        style="margin-bottom: 50px"
+      />
+    </div>
     <Dialog
       header="CREATE QUESTION"
       :visible="displayBasic"
-      style="width: 800px; padding: 20px; background: #fff"
+      style="width: 900px; padding: 20px; background: #fff"
     >
       <div style="height: 30px"></div>
-      <h3>INSERT QUESTION</h3>
+      <h3>Insert Question</h3>
       <Textarea
         style="width: 100%; padding: 10px"
-        v-model="value1"
+        ref="questionTextRef"
+        v-model="questionText"
         rows="5"
-        cols="30"
+        cols="20"
       />
 
+      <!-- <preview-question></preview-question> -->
+      <section>
+        <h3>Select Answer</h3>
+        <question-option
+          ref="firstOptionRef"
+          v-model="first"
+          label="정답"
+        ></question-option>
+        <!-- <h3>Insert the word you want to make it as "____"</h3> -->
+      </section>
+
       <section class="option-section">
-        <question-options></question-options>
+        <h3>Insert Options</h3>
+        <question-option
+          ref="firstOptionRef"
+          v-model="first"
+          label="A"
+        ></question-option>
+        <question-option
+          ref="secondOptionRef"
+          v-model="second"
+          label="B"
+        ></question-option>
+        <question-option
+          ref="thirdOptionRef"
+          v-model="third"
+          label="C"
+        ></question-option>
+        <question-option
+          ref="forthOptionRef"
+          v-model="forth"
+          label="D"
+        ></question-option>
+        <!-- <question-options></question-options> -->
+      </section>
+
+      <!-- SETTING SECTION -->
+      <section>
+        <h3 style="margin-bottom: 10px; margin-top: 20px">Options</h3>
+
+        <div style="display: flex; align-items: center; gap: 10px">
+          <InputSwitch v-model="isPublic" />
+          <span>Release Question</span>
+          <span style="margin-left: 10px"
+            >*Question will be visible to registered students.</span
+          >
+        </div>
+        <div style="display: flex; align-items: center; gap: 10px">
+          <InputSwitch v-model="isScheduled" />
+          <span>Enable Schedule</span>
+          <Calendar
+            v-model="scheduledDate"
+            showIcon
+            style="height: 35px; margin-left: 15px"
+          />
+        </div>
       </section>
 
       <template #footer>
@@ -35,23 +92,23 @@
         >
           <div>
             <Button
-              label="Preview"
+              label="PREVIEW"
               icon="pi pi-check"
-              @click="closeBasic"
+              @click="handleModalClose"
               autofocus
             />
           </div>
           <div>
             <Button
-              label="No"
+              label="CANCEL"
               icon="pi pi-times"
-              @click="closeBasic"
+              @click="handleModalClose"
               class="p-button-text"
             />
             <Button
-              label="Yes"
+              label="CREATE"
               icon="pi pi-check"
-              @click="closeBasic"
+              @click="handleCreate"
               autofocus
             />
           </div>
@@ -62,26 +119,109 @@
 </template>
 
 <script>
-import QuestionOptions from "@/components/questionOptions.vue";
+import { ref } from "vue";
+// import QuestionOptions from "@/components/questionOptions.vue";
+import PreviewQuestion from "@/components/PreviewQuestion.vue";
+
 export default {
   components: {
-    QuestionOptions,
+    // QuestionOptions,
+    PreviewQuestion,
   },
-  data() {
-    return {
-      displayBasic: false,
-      value1: "",
-      displayPosition: false,
-      position: "center",
+  setup() {
+    const isPublic = ref(false);
+    const isScheduled = ref(false);
+    const scheduledDate = ref(new Date());
+    const displayBasic = ref(false);
+    const questionText = ref("");
+
+    const questionTextRef = ref(null);
+    const firstOptionRef = ref(null);
+    const secondOptionRef = ref(null);
+    const thirdOptionRef = ref(null);
+    const forthOptionRef = ref(null);
+
+    const first = ref("");
+    const second = ref("");
+    const third = ref("");
+    const forth = ref("");
+
+    const handleModalOpen = () => {
+      displayBasic.value = true;
     };
-  },
-  methods: {
-    openBasic() {
-      this.displayBasic = true;
-    },
-    closeBasic() {
-      this.displayBasic = false;
-    },
+    const handleModalClose = () => {
+      displayBasic.value = false;
+    };
+
+    const handleCreate = () => {
+      const isValidate = validateInputFields();
+      console.log("#isValidated", isValidate);
+      const result = {
+        type: "multiple choice",
+        question: questionText.value,
+        answer: "",
+        options: [first.value, second.value, third.value, forth.value],
+        isPublick: isPublic.value,
+        isScheduled: isScheduled.value,
+      };
+      console.log("#result", result);
+    };
+
+    const validateInputFields = () => {
+      console.log(111111, questionTextRef.value);
+      console.log(222222, firstOptionRef.value);
+      if (!questionText.value) {
+        alert("입력란을 확인해주세요.");
+        return false;
+      }
+
+      if (!first.value) {
+        alert("AAAAAAAAAAA !!!");
+        firstOptionRef.value.focus();
+        return false;
+      }
+
+      if (!second.value) {
+        alert("BBBBBBB");
+        secondOptionRef.value.focus();
+        return false;
+      }
+
+      if (!third.value) {
+        alert("CCCC!!");
+        thirdOptionRef.value.focus();
+        return false;
+      }
+
+      if (!forth.value) {
+        alert("DDDDDD");
+        forthOptionRef.value.focus();
+        return false;
+      }
+
+      return true;
+    };
+
+    return {
+      isPublic,
+      isScheduled,
+      scheduledDate,
+      displayBasic,
+      questionText,
+      questionTextRef,
+      first,
+      second,
+      third,
+      forth,
+      handleModalOpen,
+      handleModalClose,
+      handleCreate,
+      validateInputFields,
+      firstOptionRef,
+      secondOptionRef,
+      thirdOptionRef,
+      forthOptionRef,
+    };
   },
 };
 </script>
