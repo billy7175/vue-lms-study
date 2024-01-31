a3b29d67f2aa4945a9b2f094a6d76a0369b4e0e3
billy7175 <whgus439@gmail.com>
2024-01-31 16:54:52 +0900

    Refactor css code in Tag.vue

--- src/components/tag/Tag.vue
+++ src/components/tag/Tag.vue
@@ -33,80 +33,71 @@ export default {
   }
 }
 </script>
-
-<style scoped>
+<style lang="scss" scoped>
 .tag-wrapper {
-  display: inline-block;
-  display: flex;
+  display: inline-flex;
   align-items: center;
   background: #daf2dd;
-  color: #4daa57;
   padding: 8px 12px;
   border-radius: 10px;
   gap: 5px;
+
+  &.is-not-active {
+    background: #f6b3b3;
+  }
+
+  .tag-text {
+    color: #4daa57;
+  }
+
+  &.is-not-active .tag-text {
+    color: #dc504f;
+  }
 }
 
-.tag-wrapper.is-not-active {
-  background: #f6b3b3;
-}
-
-.tag.is-active {
+.tag {
   display: inline-block;
   width: 15px;
   height: 15px;
-  border: 2px solid #fff;
-  background: #4daa57;
   border-radius: 50%;
+  border: 2px solid #fff;
   position: relative;
-}
 
-.tag.is-active::before {
-  content: "";
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  width: 100%;
-  height: 100%;
-  border-radius: 50%;
-  animation: circle-pulse 1.5s infinite;
-  box-shadow: 0 0 0 0 #4daa57;
-}
+  &.is-active {
+    background: #4daa57;
 
+    &::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      animation: circle-pulse 1.5s infinite;
+      box-shadow: 0 0 0 0 #4daa57;
+    }
+  }
 
-.tag-wrapper.is-not-active .tag-text {
-  color: #dc504f;
-}
-
-.tag.is-not-active {
-  display: inline-block;
-  width: 15px;
-  height: 15px;
-  border: 2px solid #fff;
-  background: #dc504f;
-  border-radius: 50%;
+  &.is-not-active {
+    background: #dc504f;
+  }
 }
 
 @keyframes circle-pulse {
   0% {
-    -webkit-transform: scale(0.95);
     transform: scale(0.95);
-    -webkit-box-shadow: 0 0 0 0 rgba(77, 170, 87, 0.9);
     box-shadow: 0 0 0 0 rgba(77, 170, 87, 0.9);
   }
 
   50% {
-    -webkit-transform: scale(1);
     transform: scale(1);
-    -webkit-box-shadow: 0 0 0 8px rgba(77, 170, 87, 0);
     box-shadow: 0 0 0 8px rgba(77, 170, 87, 0);
   }
 
   100% {
-    -webkit-transform: scale(0.95);
     transform: scale(0.95);
-    -webkit-box-shadow: 0 0 0 0 rgba(77, 170, 87, 0);
     box-shadow: 0 0 0 0 rgba(77, 170, 87, 0);
   }
 }
 </style>
-
